735e825563ae0cf42694e4362db7c547b319477c
vijaygokarn130 <vijaygokarn130>
2024-07-10 11:49:40 +0000

    push data
    push data

--- Notebook 12024-07-09 18:42:58.py
+++ Notebook 12024-07-09 18:42:58.py
@@ -12,10 +12,37 @@
 
 
 from pyspark.sql import SparkSession
+import os
 
 spark=SparkSession.builder.appName('Dataframe').getOrCreate()
 
 spark
 
 
+## read the dataset
+df_pyspark=spark.read.option('header','true').csv('/FileStore/tables/test1-1.csv',inferSchema=True)
 
+### Check the schema
+df_pyspark.printSchema()
+
+
+
+# COMMAND ----------
+
+display(df_pyspark)
+
+# COMMAND ----------
+
+df_pyspark.select(['Name','Experience']).show()
+
+
+# COMMAND ----------
+
+### Adding Columns in data frame
+df_pyspark=df_pyspark.withColumn('Experience After 2 year',df_pyspark['Experience']+2)
+df_pyspark.show()
+
+# COMMAND ----------
+
+df_pyspark=df_pyspark.drop('Experience After 2 year')
+df_pyspark.show()
