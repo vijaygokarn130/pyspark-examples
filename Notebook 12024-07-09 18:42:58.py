e1b0f2a62f1e52d45e01278e754203f76aae6378
vijaygokarn130 <vijaygokarn130>
2024-07-11 22:40:30 +0000

    adding some more commands

--- Notebook 12024-07-09 18:42:58.py
+++ Notebook 12024-07-09 18:42:58.py
@@ -1,5 +1,4 @@
 # Databricks notebook source
-# In this Video We will Cover
 # PySpark Dataframe
 # Reading The Dataset
 # Checking the Datatypes of the Column(Schema)
@@ -21,15 +20,15 @@ spark
 
 ## read the dataset
 df_pyspark=spark.read.option('header','true').csv('/FileStore/tables/test1-1.csv',inferSchema=True)
-
+df_pyspark1=spark.read.option('header','true').csv('/FileStore/tables/test3-1.csv',inferSchema=True)
 ### Check the schema
-df_pyspark.printSchema()
-
+df_pyspark1.printSchema()
+df_pyspark1.head(0)
 
 
 # COMMAND ----------
 
-display(df_pyspark)
+display(df_pyspark1.groupBy('Department'))
 
 # COMMAND ----------
 
@@ -45,4 +44,35 @@ df_pyspark.show()
 # COMMAND ----------
 
 df_pyspark=df_pyspark.drop('Experience After 2 year')
-df_pyspark.show()
+df_pyspark1.show()
+
+# COMMAND ----------
+
+df_pyspark1.groupBy('Departments').avg().show()
+
+
+# COMMAND ----------
+
+df_pyspark1.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
+df_pyspark1.withColumnRenamed('salary','compensation').show()
+
+# COMMAND ----------
+
+df_pyspark1.agg({'Salary':'sum'}).show()
+
+# COMMAND ----------
+
+display(df_pyspark1.agg({'Salary':'max'}))
+
+# COMMAND ----------
+
+df_pyspark1.groupBy('Name').max().show()
+
